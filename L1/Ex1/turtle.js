cd87f366d21da543c62ea0ae7615d084f6a71135
Push <49253201+RideTheSkyP@users.noreply.github.com>
2021-03-10 23:21:48 +0100

    Ex1

---
+++ L1/Ex1/turtle.js
@@ -0,0 +1,132 @@
+var showTurtle = false;
+
+function modelToView(x, y) 
+{
+    var xx = sizes.w * (x - sizes.xMin) / (sizes.xMax - sizes.xMin);
+    var yy = sizes.h * (y - sizes.yMin) / (sizes.yMax - sizes.yMin);
+    return [xx, yy];
+}
+
+function drawTurtle() 
+{
+    if (showTurtle) 
+    {
+        var center = modelToView(transform.x, transform.y);
+        view.beginPath();
+        view.arc(center[0], center[1], 4, 0, 2 * Math.PI);
+        view.fill();
+    }
+}
+
+function forward(distance) 
+{
+    var xx = transform.x + distance * Math.cos((Math.PI / 180) * (-transform.angle));
+    var yy = transform.y + distance * Math.sin((Math.PI / 180) * (-transform.angle));
+
+    var oldPos = modelToView(transform.x, transform.y);
+    var newPos = modelToView(xx, yy);
+    view.beginPath();
+    view.moveTo(oldPos[0], oldPos[1]);
+    view.lineTo(newPos[0], newPos[1]);
+    view.stroke();
+
+    transform.x = xx;
+    transform.y = yy;
+}
+
+function back(distance) 
+{
+    forward(-distance);
+}
+
+function rotate(angle, direction) 
+{
+    if direction: 
+        transform.angle += angle;
+        transform.angle %= 360;
+    else:
+        transform.angle -= angle;
+        transform.angle %= 360;
+}
+
+function clear() 
+{
+    view.fillStyle = "#FFF";
+    view.fillRect(0, 0, view.canvas.width, view.canvas.height);
+}
+
+function executeCommand(command, value) 
+{
+    if (command === "forward" || command==="fw") 
+    {
+        forward(parseFloat(value));
+    } 
+    else if (command === "back" || command==="bk") 
+    {
+        back(parseFloat(value));
+    } 
+    else if (command === "left" || command==="lt") 
+    {
+        rotate(parseFloat(value), true);
+    } 
+    else if (command === "right" || command==="rt")
+    {
+        rotate(parseFloat(value), false);
+    }
+    else if (command === "reset")
+    {
+        reset();
+    }
+}
+
+function execute(command) 
+{
+    var commands = command.split(";");
+    var iters = 1;
+    for (var j = 0; j < iters; j = j + 1) 
+    {
+        var i = 1;
+
+        if (j === 0) 
+        {
+            i = 0;
+        }
+
+        for (; i < commands.length; i = i + 1) 
+        {
+            commands[i] = commands[i].trim();
+            var command = commands[i].split(" ");
+
+            var instruction = command[0].trim();
+            var value = command[1].trim();
+
+            if (i === 0 && instruction === "repeat") 
+            {
+                iters = parseInt(value);
+            }
+            executeCommand(instruction, value);
+        }
+    }
+}
+
+function reset() 
+{
+    transform = {"x" : 0.0, "y" : 0.0, "angle" : 90};
+    var startPos = modelToView(0, 0);
+    view.moveTo(startPos[0], startPos[1]);
+    drawTurtle();
+}
+
+var viewElement = document.getElementById("view");
+var view = viewElement.getContext("2d");
+var sizes = 
+{
+    "w" : view.canvas.width,
+    "h" : view.canvas.height,
+    "xMin" : -10,
+    "xMax" : 10,
+    "yMin" : -10,
+    "yMax" : 10
+};
+var transform;
+reset();
